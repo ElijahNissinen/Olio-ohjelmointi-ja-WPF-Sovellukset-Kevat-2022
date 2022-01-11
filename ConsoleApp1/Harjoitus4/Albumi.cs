f10c2bff02290f14aeddb341b659e178d2246c3b
ElijahNissinen <elijahnissinen@gmail.com>
2022-01-11 09:08:30 +0200

    Harjoitus4

--- ConsoleApp1/Harjoitus4/Albumi.cs
+++ ConsoleApp1/Harjoitus4/Albumi.cs
@@ -6,5 +6,31 @@ namespace Harjoitus4
 {
     class Albumi
     {
+        private string Artisti;
+        private string Genre;
+        private string Hinta;
+
+        private List<Kappale> kappaleet = new List<Kappale>();
+
+        public Albumi(string artisti, string genre, string hinta, List<Kappale> kappaleet)
+        {
+            Artisti = artisti;
+            Genre = genre;
+            Hinta = hinta;
+            this.kappaleet = kappaleet;
+        }
+
+        public void LisääKappale(Kappale uusiKappale)
+        {
+            kappaleet.Add(uusiKappale);
+        }
+
+        public void TulostaKappaleet()
+        {
+            foreach (Kappale kappale in kappaleet)
+            {
+                Console.WriteLine(kappale.Nimi + ", " + kappale.Pituus);
+            }
+        }
     }
 }
--- ConsoleApp1/Harjoitus4/Program.cs
+++ ConsoleApp1/Harjoitus4/Program.cs
@@ -6,7 +6,12 @@ namespace Harjoitus4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine ()
+            
+            Albumi albumi = new Albumi();
+            albumi.LisääKappale(new Kappale("Kappale1", "03:33"));
+            albumi.LisääKappale(new Kappale("Kappale2", "04:04"));
+            albumi.LisääKappale(new Kappale("Kappale3", "04:00"));
         }
     }
 }
