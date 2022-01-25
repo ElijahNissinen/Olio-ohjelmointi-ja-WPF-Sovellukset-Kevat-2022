31e958113becbb1d35f152deed7c76af1359191e
ElijahNissinen <elijahnissinen@gmail.com>
2022-01-25 10:20:43 +0200

    harjoitus23

---
+++ ConsoleApp1/Harjoitus23/Kiuas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus23
+{
+    class Kiuas
+    {
+        public int Lämpötila { get; set; }
+        public int Kosteus { get; set; }
+        public bool OnkoPäällä { get; set; }
+
+        public void VaihdaLämpötila(int UusiLämpötila)
+        {
+            Lämpötila = UusiLämpötila;
+
+            if (Lämpötila > 120)
+                Lämpötila = 120;
+
+            if (Lämpötila < 10)
+                  Lämpötila = 10;
+        }
+        public void VaihdaKosteus(int UusiKosteus)
+        {
+            Kosteus = UusiKosteus;
+
+            if (Lämpötila > 100)
+                Lämpötila = 100;
+
+            if (Lämpötila < 0)
+                Lämpötila = 0;
+        }
+    }
+}
---
+++ ConsoleApp1/Harjoitus23/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus23
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Kiuas kiuas = new Kiuas();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //alustetaan kiuas tietyillä arvoilla
+            kiuas.VaihdaLämpötila(10);
+            kiuas.VaihdaKosteus(10);
+            kiuas.OnkoPäällä = false; //kiuas on alussa false/poissa päältä
+        }
+
+        private void btn_LämpötilaUp_Click(object sender, RoutedEventArgs e)
+        {
+            int UusiLämpötila = kiuas.Lämpötila + 5; //lisätään lämpötilaan 5
+            kiuas.VaihdaLämpötila(UusiLämpötila); //kutsutaan kiukaan toimintoa jossa tarkistetaan onko se 10-120 välillä
+            tb_Lämpötila.Text = kiuas.Lämpötila.ToString(); //lisätään lämpötila tekstikenttään nykyinen lämpötila
+        }
+
+        private void btn_LämpötilaDown_Click(object sender, RoutedEventArgs e)
+        {
+            int UusiLämpötila = kiuas.Lämpötila - 5; //vähennetään lämpötilasta 5
+            kiuas.VaihdaLämpötila(UusiLämpötila); //kutsutaan kiukaan toimintoa jossa tarkistetaan onko se 10-120 välillä
+            tb_Lämpötila.Text = kiuas.Lämpötila.ToString(); //lisätään lämpötila tekstikenttään nykyinen lämpötila
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //tämän avulla kosteus textbox ottaa sisään VAIN numeroita, kirjaimia ja muita merkkejä ei voi kirjoittaa
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void input_Kosteus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (input_Kosteus.Text != "") // != tarkoittaa että a EI OLE YHTÄKUIN b
+            {
+                kiuas.VaihdaKosteus(int.Parse(input_Kosteus.Text));
+                tb_Kosteus.Text = kiuas.Kosteus.ToString();
+            }
+        }
+    }
+}
